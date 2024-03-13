5751827b7e3d4b4ac84849a454f9dca7225301d1
ragnika <ragnika@users.noreply.github.com>
2024-03-13 15:57:53 +1100

    export jsonpath functions (#26)
    Co-authored-by: ragnika <fendy.putra@meeco.me>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './common.js';
 export * from './helpers.js';
 export * from './issuer.js';
+export * from './jsonpath.js';
 export * from './types.js';
 export * from './verifier.js';
